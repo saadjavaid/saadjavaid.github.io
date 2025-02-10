35605bd28a4bfa95e72a25df8bdab57aa0afdf28
Saad Javaid <32400297+saadjavaid@users.noreply.github.com>
2025-02-10 23:42:58 +0500

    Changes to GA4 Implemetation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-import partytown from "@astrojs/partytown";
+import { partytown } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
 export default defineConfig({
 	site: 'https://saadjavaid.github.io',
-	integrations: [tailwind(),partytown()],
+	integrations: [tailwind(),partytown({ config: { forward: ["dataLayer.push"] } })],
 });
